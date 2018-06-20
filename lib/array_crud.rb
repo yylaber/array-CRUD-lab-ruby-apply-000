5fc3fb9e30d282c6d1008c6fa927c8a213d60bc9
Yosef Levertov <yylaber@gmail.com>
2018-06-20 18:24:48 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,10 @@ end
 
 def add_element_to_end_of_array(array, element)
   array << element
-  #  = ["wow", "I", "am", "really", "learning"]
-
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
